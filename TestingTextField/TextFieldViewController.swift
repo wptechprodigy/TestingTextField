a2fedcd12c6e4fa6dd19b1c1a041d5faab67d721
Waheed Afolabi <waheed.freelancer16@gmail.com>
2021-11-25 10:55:09 +0100

    prevent user from entering empty space in the textfields and controlling the focus on the textfields

--- TestingTextField/TextFieldViewController.swift
+++ TestingTextField/TextFieldViewController.swift
@@ -19,4 +19,34 @@ class TextFieldViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    // MARK: - Object Methods
+    
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        if textField === usernameField {
+            return !string.contains(" ")
+        } else {
+            return true
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField === usernameField {
+            passwordField.becomeFirstResponder()
+        } else {
+            guard let userName = usernameField.text,
+                  let password = passwordField.text else { return false }
+            
+            passwordField.resignFirstResponder()
+            performLogin(username: userName, password: password)
+        }
+        return false
+    }
+    
+    private func performLogin(username: String, password: String) {
+        print(">> Username: \(username)")
+        print(">> Password: \(password)")
+    }
 }
